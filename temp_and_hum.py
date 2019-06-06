a85ecd8fbbab2ff2956a0553a7a62d292e041553
Devin Klepp <33706676+DevinKlepp@users.noreply.github.com>
2019-06-06 19:53:18 -0400

    Added new file which communicates with AWS

--- temp_and_hum.py
+++ temp_and_hum.py
@@ -2,16 +2,72 @@
 # Adafruit Python DHT Sensor Library
 # https://github.com/adafruit/Adafruit_Python_DHT
 
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 import time
 import datetime
 import Adafruit_DHT
 
+SHADOW_CLIENT = "myShadowClient"
+
+# Hidden host name (Modified for security)
+HOST_NAME = "XXXXXXXXXXXXXXXXXXXXXX.amazonaws.com"
+
+# Path to root CA file
+ROOT_CA = "AmazonRootCA1.pem"
+
+# Path to private key file
+PRIVATE_KEY = "XXXXXXXXXXXXprivate.pem.key"
+
+# Path to certificate file
+CERT_FILE = "XXXXXXXXXXXXXXXXXXXXX.pem.crt.txt"
+#"yourkeyid-certificate.pem.crt.txt"
+
+# AWS IoT thing name, my RaspberryPi device
+SHADOW_HANDLER = "MyRPi"
+
+###########################################################
+
+# Automatically called whenever the shadow is updated.
+def myShadowUpdateCallback(payload, responseStatus, token):
+    print()
+    print('UPDATE: $aws/things/' + SHADOW_HANDLER +
+        '/shadow/update/#')
+    print("payload = " + payload)
+    print("responseStatus = " + responseStatus)
+    print("token = " + token)
+
+
+# Create, configure, and connect a shadow client.
+myShadowClient = AWSIoTMQTTShadowClient(SHADOW_CLIENT)
+myShadowClient.configureEndpoint(HOST_NAME, 8883)
+myShadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERT_FILE)
+myShadowClient.configureConnectDisconnectTimeout(10)
+myShadowClient.configureMQTTOperationTimeout(5)
+myShadowClient.connect()
+
+
+# Create a programmatic representation of the shadow.
+myDeviceShadow = myShadowClient.createShadowHandlerWithName(
+    SHADOW_HANDLER, True)
+
 
 sensor = Adafruit_DHT.DHT11
 pin = 17
+
 while True:
+    
+    # Collecting data from sensor
     hum, temp = Adafruit_DHT.read_retry(sensor, pin)
-    print("The temperature is " + str(temp) + "C, " + str(temp * (9.0 / 5.0) + 32.0) + "F with a humidity of " + str(hum) + "%")
-    currenttime = datetime.datetime.now()
-    print("Recorded at " +  str(currenttime))
-    time.sleep(5)
+    myDeviceShadow.shadowUpdate(
+       '{"state":{"reported":{"Temperature":' + str(temp) +
+       ', "Humidity":' + str(hum) + '}}}',
+      myShadowUpdateCallback, 5)
+    #print("\n")
+    """
+    myDeviceShadow.shadowUpdate(
+       '{"state":{"reported":{"Humidity":' + str(hum) + '}}}',
+      myShadowUpdateCallback, 5)
+      """
+    # Print statements
+    print("\nThe temperature is " + str(temp) + "C, with a humidity of " + str(hum) + "%")
+    time.sleep(20)
